32198bc645fd19c6ee04a5cfa575ae1081852934
Avielbe <avielmbe@gmail.com>
2025-07-15 12:21:15 +0300

    home component: testing new UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import animate from 'tailwindcss-animate';
+
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: { extend: {} },
-  plugins: [],
+  plugins: [animate],
 }
 
